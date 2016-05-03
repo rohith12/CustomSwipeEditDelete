6ce3d99c6b1aeaba29f00b938fdd9800dbd0adf5
Rohith <r12.rohith@gmail.com>
2016-05-03 10:41:48 +0530

    Initial Commit

---
+++ MVCdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVCdemo
+//
+//  Created by Rohith on 03/05/16.
+//  Copyright © 2016 rohith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MVCdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVCdemo
+//
+//  Created by Rohith on 03/05/16.
+//  Copyright © 2016 rohith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
